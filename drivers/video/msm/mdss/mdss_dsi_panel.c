c7c822bf5d8ece4f2147889abf2dea1d8dff0d74
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2015-10-20 16:39:28 +0530

    msm: mdss: handle DSI ctrl/PHY regulator control properly
    Power on/off of the DSI regulators is currently tied to the state
    of the DSI Panel as part of DSI clock control API. During
    mdss_dsi_on(), the panel will still be blanked but DSI is powered
    on. Due to the current logic, we will end up powering down the DSI
    ctrl/PHY regulators at the end of mdss_dsi_on() for command mode
    panels when we turn off the DSI core and link clocks. This might
    cause an abnormal waveform during resume on DSI clock/data lanes
    until we power ON back the DSI regulators as part of DSI panel
    unblank. Fix this by using the DSI controller state variable instead
    of DSI panel state.
    
    Change-Id: Icfa3910f7204c3db63e21a9c9ba082430c0fdf56
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -767,7 +767,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	if (ctrl->ds_registered)
 		mdss_dba_utils_video_on(pinfo->dba_data, pinfo);
 end:
-	pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 	pr_debug("%s:-\n", __func__);
 	return ret;
 }
@@ -849,7 +848,6 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	}
 
 end:
-	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
 	pr_debug("%s:-\n", __func__);
 	return 0;
 }
@@ -873,10 +871,6 @@ static int mdss_dsi_panel_low_power_config(struct mdss_panel_data *pdata,
 		enable);
 
 	/* Any panel specific low power commands/config */
-	if (enable)
-		pinfo->blank_state = MDSS_PANEL_BLANK_LOW_POWER;
-	else
-		pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 
 	pr_debug("%s:-\n", __func__);
 	return 0;
